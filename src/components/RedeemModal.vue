fc622c6b3b75b8ced5d1530940934f08d54adb5a
cherent tsegaye <abenigas59@gmail.com>
2026-04-04 19:42:12 +0300

    a

--- src/components/RedeemModal.vue
+++ src/components/RedeemModal.vue
@@ -405,7 +405,7 @@ function bgHelper(t) {
       formatted3 === t.name ||
       formatted4 === t.name ||
       formatted5 === t.name ||
-      formatted6 === bet.name
+      formatted6 === t.name
     ) {
       return 1;
     }
